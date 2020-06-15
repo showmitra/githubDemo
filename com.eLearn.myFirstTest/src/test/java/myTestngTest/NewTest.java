88880c4cba7516606639eaa1b0f4976c24d9b9a4
showmitra <shownaf@gmail.com>
2020-06-15 13:40:45 -0400

    This is the initial project commit to the Local repository.
    Change-Id: I4de4f505f24a2ae2444b2e52a282595396f2ccbb
    Signed-off-by: showmitra <shownaf@gmail.com>

---
+++ com.eLearn.myFirstTest/src/test/java/myTestngTest/NewTest.java
@@ -0,0 +1,15 @@
+package myTestngTest;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	 @Test
+	  public void myLoginTest() {
+		  System.out.println("My login test");
+	  }
+	  
+	  @Test
+	  public void myLogOutTest() {
+		  System.out.println("My logout test");
+	  }
+}
---
+++ gitHubDemo/src/gitHubDemo/TestClass.java
@@ -0,0 +1,10 @@
+package gitHubDemo;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is github project");
+
+	}
+
+}
